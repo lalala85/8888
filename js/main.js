fa43efaa7894ad8f5254ad8864c7e337d34f6491
lalala85 <94925121+lalala85@users.noreply.github.com>
2021-11-24 17:57:41 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,146 @@
+$(document).ready(function() {
+
+	/* wow js animations activation */
+	new WOW().init();
+
+	/* Navigation burger onclick side navigation show */
+	$('.burger-container').on('click', function() {
+		$('.main-navigation').toggle('slow');
+
+		if($('#myBtn').hasClass('change')) {
+			$('body').addClass('stop-scroll');
+		} else {
+			$('body').removeClass('stop-scroll');
+		}
+	});
+
+
+	/* About me slider */
+	$('.about-me-slider').slick({
+		slidesToShow: 1,
+		prevArrow: '<span class="span-arrow slick-prev"><</span>',
+		nextArrow: '<span class="span-arrow slick-next">></span>'
+	});
+
+	/* Blog slider */
+	$('.blog-slider').slick({
+		slidesToShow: 2,
+		prevArrow: '<span class="span-arrow slick-prev"><</span>',
+		nextArrow: '<span class="span-arrow slick-next">></span>',
+		responsive: [
+		{
+			breakpoint: 768,
+			settings: {
+				slidesToShow: 1
+			}
+		}
+		]
+	});
+
+
+	/* Contact form validation */
+	var contactForm = function() {
+		if ($('#contactForm').length > 0 ) {
+			$( "#contactForm" ).validate( {
+				rules: {
+					name: "required",
+					email: {
+						required: true,
+						email: true
+					},
+					message: {
+						required: true,
+						minlength: 5
+					}
+				},
+				messages: {
+					name: "Please enter your name",
+					email: "Please enter a valid email address",
+					message: "Please enter a message"
+				},
+				/* submit via ajax */
+				submitHandler: function(form) {   
+					var $submit = $('.submitting'),
+					waitText = 'Submitting...';
+
+					$.ajax({    
+						type: "POST",
+						url: "php/sendEmail.php",
+						data: $(form).serialize(),
+
+						beforeSend: function() { 
+							$submit.css('display', 'block').text(waitText);
+						},
+						success: function(msg) {
+							if (msg == 'OK') {
+								$('#form-message-warning').hide();
+								setTimeout(function(){
+									$('#contactForm').fadeOut();
+								}, 1000);
+								setTimeout(function(){
+									$('#form-message-success').fadeIn();   
+								}, 1400);
+
+							} else {
+								$('#form-message-warning').html(msg);
+								$('#form-message-warning').fadeIn();
+								$submit.css('display', 'none');
+							}
+						},
+						error: function() {
+							$('#form-message-warning').html("Something went wrong. Please try again.");
+							$('#form-message-warning').fadeIn();
+							$submit.css('display', 'none');
+						}
+					});       
+				}
+
+			} );
+		}
+	};
+
+
+	$(function(){
+		contactForm();
+	});
+
+
+});
+
+
+
+var counta = 0;
+
+$(window).scroll(function(e){
+
+
+	/* Onscroll number counter */
+	var statisticNumbers = $('.single-count');
+	if(statisticNumbers.length) {
+		var oTop = statisticNumbers.offset().top - window.innerHeight;
+		if (counta == 0 && $(window).scrollTop() > oTop) {
+			$('.count').each(function() {
+				var $this = $(this),
+				countTo = $this.attr('data-count');
+				$({
+					countNum: $this.text()
+				}).animate({
+					countNum: countTo
+				},
+
+				{
+					duration: 2000,
+					easing: 'swing',
+					step: function() {
+						$this.text(Math.floor(this.countNum));
+					},
+					complete: function() {
+						$this.text(this.countNum);
+					}
+				});
+			});
+			counta = 1;
+		}
+	}
+
+});
